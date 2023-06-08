c46f7922db30b4431900f30645161e2b821ec24e
Akash Verma <75831162+akashazad-dot@users.noreply.github.com>
2023-06-08 22:45:27 +0530

    Arithmetic Number.cpp

---
+++ Arithmetic Number.cpp
@@ -0,0 +1,15 @@
+class Solution{
+public:
+    int inSequence(int A, int B, int C){
+        if(C==0){
+            return(A==B);
+        }
+        
+        if( (B-A)%C==0 && (B-A)/C>=0){
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+};
